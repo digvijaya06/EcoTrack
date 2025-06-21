248e7a53db0b42a5c72c734dfedadf10d7cbe1a0
Digvijaya-lyrical <digvijayanagda@gmail.com>
2025-06-21 22:09:45 +0530

    `Update .gitignore and package.json in server directory`

--- server/server.js
+++ server/server.js
@@ -89,10 +89,7 @@ const PORT = process.env.PORT || 5000;
 
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB connected");
 
